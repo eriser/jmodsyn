60679147a47ac9cc9a1b2d573f4b6102ff823e71
eduijs <eduijs@8a8c6d3c-d791-453b-a0f7-9bec5c4596bb>
2015-04-13 15:07:16 +0000

    DspPatchCombination, tabs in editor
    git-svn-id: svn://svn.code.sf.net/p/jmodsyn/code@60 8a8c6d3c-d791-453b-a0f7-9bec5c4596bb

--- trunk/ModSyn-j2se/src/org/modsyn/editor/MetaConnectionLabel.java
+++ trunk/ModSyn-j2se/src/org/modsyn/editor/MetaConnectionLabel.java
@@ -6,6 +6,11 @@ import javax.swing.JLabel;
 
 public class MetaConnectionLabel extends JLabel {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3273306472666033456L;
+
 	public MetaConnectionLabel(String text, int horizontalAlignment) {
 		super(text, horizontalAlignment);
 	}
@@ -18,9 +23,7 @@ public class MetaConnectionLabel extends JLabel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(getParent().getBackground());
-		// @formatter:off
 		g.fillPolygon(new int[] { getWidth() - (getHeight() / 2), getWidth(), getWidth(), getWidth() - (getHeight() / 2), getWidth() }, new int[] { 0, 0,
 				getHeight(), getHeight(), getHeight() / 2 }, 5);
-		// @formatter:on
 	}
 }
