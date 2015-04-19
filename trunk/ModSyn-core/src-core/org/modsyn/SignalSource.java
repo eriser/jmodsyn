0f0c3616116f586045af4020b9ab6bf5144757ce
eduijs <eduijs@8a8c6d3c-d791-453b-a0f7-9bec5c4596bb>
2015-04-19 20:27:13 +0000

    javadoc
    git-svn-id: svn://svn.code.sf.net/p/jmodsyn/code@76 8a8c6d3c-d791-453b-a0f7-9bec5c4596bb

--- trunk/ModSyn-core/src-core/org/modsyn/SignalSource.java
+++ trunk/ModSyn-core/src-core/org/modsyn/SignalSource.java
@@ -4,8 +4,10 @@
 package org.modsyn;
 
 /**
- * An SignalOutputSource is a SignalOutput that is updated at every sample. This
- * typically means a signal source, for example an Oscillator.
+ * An SignalSource is a SignalOutput that is updated at every sample. This typically means a signal source, for example
+ * an Oscillator.<br>
+ * A DspObject that is not a SignalSource is typically updated when it receives a signal on a SignalInput (and/or in
+ * some cases an external event, such as a MIDI event or GUI event).
  * 
  * @author Erik Duijs
  */
