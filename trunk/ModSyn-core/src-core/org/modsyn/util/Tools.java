0bab6d7337418578cb4fba6b73313e320bd73745
eduijs <eduijs@8a8c6d3c-d791-453b-a0f7-9bec5c4596bb>
2015-08-20 17:22:44 +0000

    fast tanh
    git-svn-id: svn://svn.code.sf.net/p/jmodsyn/code@108 8a8c6d3c-d791-453b-a0f7-9bec5c4596bb

--- trunk/ModSyn-core/src-core/org/modsyn/util/Tools.java
+++ trunk/ModSyn-core/src-core/org/modsyn/util/Tools.java
@@ -8,7 +8,11 @@ package org.modsyn.util;
 
 import static java.lang.Math.pow;
 
+import java.util.Random;
+
 public class Tools {
+
+	public static final boolean FAST_MATH = true;
 
 	public static final float[] FREQUENCY = new float[128];
 	public static final double LOG2 = Math.log(2);
@@ -27,7 +31,45 @@ public class Tools {
 		return (int) (Math.log(freq) / LOG2);
 	}
 
+	public static float tanh(float f) {
+		if (FAST_MATH) {
+			float xa = Math.abs(f);
+			float x2 = xa * xa;
+			float x3 = xa * x2;
+			float x4 = x2 * x2;
+			float x7 = x3 * x4;
+			float res = (1.0f - 1.0f / (1.0f + xa + x2 + 0.58576695f * x3 + 0.55442112f * x4 + 0.057481508f * x7));
+			return (f > 0 ? res : -res);
+		} else {
+			return (float) Math.tanh(f);
+		}
+	}
+
 	public static void main(String[] args) {
+		for (float f = 0; f < 10; f += 0.25f) {
+			System.out.println("tanh(" + f + ") = \t" + ((float) Math.tanh(f)) + " \t~ " + tanh(f));
+			System.out.println(" tanh(" + (-f) + ") = \t" + ((float) Math.tanh(-f)) + " \t~ " + tanh(-f));
+		}
+
+		Random rng = new Random(1);
+
+		float f = 0;
+		long start = System.nanoTime();
+		for (int i = 0; i < 10000000; i++) {
+			f += Math.tanh(rng.nextFloat());
+		}
+		System.out.println(((System.nanoTime() - start) / 1000000f) + " time " + f);
+		f = 0;
+		start = System.nanoTime();
+		for (int i = 0; i < 10000000; i++) {
+			f += tanh(rng.nextFloat());
+		}
+		System.out.println(((System.nanoTime() - start) / 1000000f) + " time " + f);
+
+		testOctaves();
+	}
+
+	private static void testOctaves() {
 		System.out.println(getOctave(1));
 		System.out.println(getOctave(2));
 		System.out.println(getOctave(4));
